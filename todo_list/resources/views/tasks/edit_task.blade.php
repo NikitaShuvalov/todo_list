36ac1752de64d4d17054b41bbc59008b97d38993
NikitaShuvalov <ktototam065@gmail.com>
2023-10-03 15:11:57 +0600

    created edit task template

---
+++ todo_list/resources/views/tasks/edit_task.blade.php
@@ -0,0 +1,18 @@
+@extends("layout")
+
+@section("title")
+    Edit task
+@endsection
+
+@section("content")
+    <h1>Edit task</h1>
+    <hr>
+    <form action="/edit/{{$task->id}}" method="post">
+        @csrf
+        <span>Title:</span>
+        <input type="text" value="{{ $task->title }}" name="title" class="form-control"><br>
+        <span>Description:</span>
+        <textarea class="form-control" name="description">{{ $task->description }}</textarea><br>
+        <button class="btn btn-primary" type="submit">Edit</button>
+    </form>
+@endsection
